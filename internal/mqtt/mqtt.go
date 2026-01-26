4209a4150a160519c55a32704f1f2c9ae5cd0e37
Kacper Miklaszewski <kkmiklaszewski@gmail.com>
2026-01-26 06:21:08 +0000

    mqtt with connect function and timeouts

--- internal/mqtt/mqtt.go
+++ internal/mqtt/mqtt.go
@@ -1,7 +1,9 @@
 package mqtt
 
 import (
+	"fmt"
 	"log/slog"
+	"time"
 
 	MQTT "github.com/eclipse/paho.mqtt.golang"
 )
@@ -10,13 +12,13 @@ type MQTTClient struct {
 	client MQTT.Client
 }
 
-func New(o *MQTT.ClientOptions) (*MQTTClient, error) {
+func New(o *MQTT.ClientOptions) *MQTTClient {
 	o.OnConnect = func(c MQTT.Client) {
 		slog.Info("mqtt connected")
 	}
 
 	o.OnConnectionLost = func(c MQTT.Client, err error) {
-		slog.Error("mqtt connection lost", "err", err)
+		slog.Warn("mqtt connection lost", "err", err)
 	}
 
 	o.OnReconnecting = func(c MQTT.Client, co *MQTT.ClientOptions) {
@@ -24,18 +26,25 @@ func New(o *MQTT.ClientOptions) (*MQTTClient, error) {
 	}
 
 	client := MQTT.NewClient(o)
-	if token := client.Connect(); token.Wait() && token.Error() != nil {
-		return nil, token.Error()
-	}
 
 	return &MQTTClient{
 		client: client,
-	}, nil
+	}
+}
+
+func (m *MQTTClient) Connect(timeout time.Duration) error {
+	token := m.client.Connect()
+	if !token.WaitTimeout(timeout) {
+		return fmt.Errorf("mqtt connect timeout after %s", timeout)
+	}
+	return token.Error()
 }
 
 func (m *MQTTClient) Publish(topic string, qos byte, retain bool, payload []byte) error {
 	token := m.client.Publish(topic, qos, retain, payload)
-	token.Wait()
+	if !token.WaitTimeout(5 * time.Second) {
+		return fmt.Errorf("mqtt publish timeout (topic=%s)", topic)
+	}
 	return token.Error()
 }
 
