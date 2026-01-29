2249c8b173492dfe05da1ea95f2457adb7431675
Kacper Miklaszewski <kkmiklaszewski@gmail.com>
2026-01-29 05:43:47 +0000

    Sensors to be added first

--- internal/sensors/registry.go
+++ internal/sensors/registry.go
@@ -1,24 +1,85 @@
 package sensors
 
 var Sensor = map[string]SensorDefinition{
+	// CPU
 	"cpu_usage": {
 		DefaultName: "CPU usage",
-		DefaultIcon: "",
+		DefaultIcon: "mdi:cpu-64-bit",
+		DefaultUnit: "%",
+	},
+	"cpu_load": {
+		DefaultName: "CPU load",
+		DefaultIcon: "mdi:chart-line",
 		DefaultUnit: "",
 	},
+	"cpu_temp": {
+		DefaultName: "CPU temperature",
+		DefaultIcon: "mdi:thermometer",
+		DefaultUnit: "°C",
+	},
+
+	// System
+	"uptime": {
+		DefaultName: "System uptime",
+		DefaultIcon: "mdi:timer-outline",
+		DefaultUnit: "s",
+	},
+	"os_version": {
+		DefaultName: "OS version",
+		DefaultIcon: "mdi:linux",
+		DefaultUnit: "",
+	},
+
+	// Memory
 	"memory_usage": {
-		DefaultName: "RAM usage",
-		DefaultIcon: "",
-		DefaultUnit: "",
+		DefaultName: "Memory usage",
+		DefaultIcon: "mdi:memory",
+		DefaultUnit: "%",
 	},
+	"swap_usage": {
+		DefaultName: "Swap usage",
+		DefaultIcon: "mdi:swap-horizontal",
+		DefaultUnit: "%",
+	},
+
+	// Disk
 	"disk_usage": {
 		DefaultName: "Disk usage",
-		DefaultIcon: "",
+		DefaultIcon: "mdi:harddisk",
+		DefaultUnit: "%",
+	},
+
+	// Network
+	"public_ip": {
+		DefaultName: "Public IP",
+		DefaultIcon: "mdi:ip",
 		DefaultUnit: "",
 	},
 	"wifi_signal": {
-		DefaultName: "WiFi signal",
-		DefaultIcon: "",
-		DefaultUnit: "",
+		DefaultName: "Wi-Fi signal",
+		DefaultIcon: "mdi:wifi",
+		DefaultUnit: "%",
+	},
+
+	// GPU
+	"gpu_usage": {
+		DefaultName: "GPU usage",
+		DefaultIcon: "mdi:gpu",
+		DefaultUnit: "%",
+	},
+	"gpu_memory_usage": {
+		DefaultName: "GPU memory usage",
+		DefaultIcon: "mdi:memory",
+		DefaultUnit: "%",
+	},
+	"gpu_temp": {
+		DefaultName: "GPU temperature",
+		DefaultIcon: "mdi:thermometer",
+		DefaultUnit: "°C",
+	},
+	"gpu_power": {
+		DefaultName: "GPU power",
+		DefaultIcon: "mdi:flash",
+		DefaultUnit: "W",
 	},
 }
